5049cdb332cf209c34db119a9a1b6dfbbc7b96e3
Szymioneko <szymonowok005@gmail.com>
2021-12-08 10:59:29 +0100

    Aktualizacja plików

--- sprawdz.php
+++ sprawdz.php
@@ -1,12 +1,11 @@
 <?php
 $zmienna = $_GET['stan'];
 
-fi($zmienna==1){
+if($zmienna==1){
     echo("Zmienna: ".$zmienna);
 }else if($zmienna==2){
     echo("Zmienna inna: ".$zmienna);
 }else{
-    echo("Brak danych do pobrania")
+    echo("Brak danych do pobrania"); 
 }
-
 ?>
